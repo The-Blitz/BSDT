308c0531a7559023654b124b0bd61db9cefcae62
The-Blitz <a20112387@pucp.pe>
2017-02-04 14:48:01 -0500

    Correciones, errores python2/python3

--- sentences.py
+++ sentences.py
@@ -5,24 +5,36 @@ import numpy as np;
 def verif(cont, text):
 	auxSense=""
 	for i in range(cont, len(text)):
-		if(auxSense==""):	senses= dbc.senseSearch(text[i])
-		else:			senses= dbc.senseSearch(auxSense+"_" +text[i])
-		if(len(senses)==0): return auxSense,i-1
-		if(auxSense==""):	auxSense= text[i]
-		else:			auxSense= auxSense+"_" +text[i]
+		if(auxSense==""):
+			senses= dbc.senseSearch(text[i])
+		else:
+			senses= dbc.senseSearch(auxSense+"_" +text[i])	
+		if( (len(senses)==0 ) or (i>cont and senses[0][0][len(auxSense)] != "_" )  ):
+			#checking words and phrases
+			return auxSense,i
+		if(auxSense==""):
+			auxSense= text[i]
+		else:
+			auxSense= auxSense+"_" +text[i]
+	return auxSense, len(text)
 
-	return auxSense, len(text) ;
+
 def procSentence(text):
     sz= len(text)
-    vis= np.zeros(sz)	
+    vis= np.zeros(sz)
     result=[];
-    for i in range (sz):
-	if(vis[i]!=0):	continue;
-	word , counter = verif(i,text)
-	for j in range(i,counter):	vis[j]=1
-	if(word!=""): result.append(word)	
+    for i in range(sz):
+    	if vis[i]!=0:
+    		continue
+    	word,counter = verif(i,text)
+    	for j in range(i,counter):
+    		vis[j]=1
+    	if word :
+    		result.append(word)
     return result
+   
 
 sentence = ["Vería" , "esa" , "película" , "una" , "y" , "otra" , "vez"]
+#sentence  = ["dime" , "si" , "te", "sientes" , "cansada"]
 result=procSentence(sentence)
 print (result)
