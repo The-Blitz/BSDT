c1fcc5d8a5ad529539d712ee7059b542e45dd60d
The-Blitz <a20112387@pucp.pe>
2017-02-06 16:48:42 -0500

    pequeños cambios

--- freelingUser.py
+++ freelingUser.py
@@ -1,13 +1,13 @@
 import sys
 from Freeling import freeling;
 
-def procText (text):
+def initFreeling():
 	FREELINGDIR = "/usr/local";
 	DATA = FREELINGDIR+"/share/freeling/";
 	LANG="es";
 
 	freeling.util_init_locale("default");
-
+	
 	# create options set for maco analyzer. Default values are Ok, except for data files.
 	op= freeling.maco_options("es");
 	op.set_data_files( "", 
@@ -23,7 +23,7 @@ def procText (text):
 	# create analyzers
 	tk=freeling.tokenizer(DATA+LANG+"/tokenizer.dat");
 	sp=freeling.splitter(DATA+LANG+"/splitter.dat");
-	sid=sp.open_session();
+	#sid=sp.open_session();
 	mf=freeling.maco(op);
 
 	# activate mmorpho odules to be used in next call
@@ -34,13 +34,11 @@ def procText (text):
 	# create tagger, sense anotator, and parsers
 	tg=freeling.hmm_tagger(DATA+LANG+"/tagger.dat",True,2);
 	sen=freeling.senses(DATA+LANG+"/senses.dat");
-	
-	l = tk.tokenize(text);
-	ls = sp.split(sid,l,True);
-	ls = mf.analyze(ls);
-	ls = tg.analyze(ls);
-	ls = sen.analyze(ls);
-	
+
+	return (tk,sp,mf,tg,sen)
+
+def procText (text):
+	ls= analyze(text)
 	words=[]	
 	lemmas=[]
 	tags=[]
@@ -53,9 +51,19 @@ def procText (text):
 			#print(w.get_form()+" "+w.get_lemma()+" "+w.get_tag());
 	#print ("");
 
-	sp.close_session(sid);
 	return words,lemmas,tags;
 
+def analyze(text):
+	tk,sp,mf,tg,sen = initFreeling()
+	sid=sp.open_session();
+	l = tk.tokenize(text);
+	ls = sp.split(sid,l,True);
+	ls = mf.analyze(ls);
+	ls = tg.analyze(ls);
+	ls = sen.analyze(ls);
+	sp.close_session(sid);
+	return ls;
+	
 #procText("Esto es bastante estupido")
 #words,lemmas,tags=procText("Vería esa película una_y_otra_vez")
 #print (words,lemmas,tags)
