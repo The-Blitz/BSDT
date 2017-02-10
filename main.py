4c5cb6a0afe901932cb6a466bb7dda0418ac933b
The-Blitz <a20112387@pucp.pe>
2017-02-09 19:43:29 -0500

    modificando lectura, revisar conflictos freeling/mcr30

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import freelingUser as fu;
 
 
 #falta split (para más oraciones y tokenizar cada una)
-#sentence = ["Vería" , "esa" , "película" , "una" , "y" , "otra" , "vez" , "sin" , "parar"]
+sentence = ["Vería" , "esa" , "película" ,"de" , "baja", "estofa", "una" , "y" , "otra" , "vez" , "sin" , "parar"]
 
-#result=s.procSentence(sentence)
+result=s.procSentence(sentence)
+words,lemmas,tags=fu.procText(result)
+print (words,lemmas,tags)
 
-#words,lemmas,tags=fu.procText(result)
-#print (words,lemmas,tags)
+
+fileName = 'Corpus/spanish_objectives_filmaffinity_2500'
+objFile = s.readFile(fileName,'latin-1')
+fileName = 'Corpus/spanish_subjectives_filmaffinity_2500'
+subjFile  =s.readFile(fileName,'utf-8')
 
 
-#fileName = 'Corpus/spanish_objectives_filmaffinity_2500'
-#readFile(fileName)
-#fileName = 'Corpus/spanish_subjectives_filmaffinity_2500'
-#s.readFile(fileName)
+#for i in range(len(subjFile)):
+#	aux = fu.procText(subjFile[i])
+#	print (aux)
+
+#aux = fu.procText(subjFile[0])
+#print (aux)
+#print (s.procSentence(aux[0]))
--- sentences.py
+++ sentences.py
@@ -13,8 +13,13 @@ def verif(cont, text):
 			senses= dbc.senseSearch(auxSense+"_" +text[i])	
 		if( (len(senses)==0 ) or (i>cont and senses[0][0][len(auxSense)] != "_" )  ):
 			if(i==cont):	#unchecked words
-				return (text[i],i+1)
-			#checking words and phrases
+				return text[i],i+1
+				
+			previous= dbc.senseSearch(auxSense)
+			if(not existSense (previous , auxSense)): #checking if the current sense is valid	
+				return text[cont], cont+1
+				
+			#return valid sense
 			return auxSense,i
 		if(auxSense==""):
 			auxSense= text[i]
@@ -22,6 +27,11 @@ def verif(cont, text):
 			auxSense= auxSense+"_" +text[i]
 	return auxSense, len(text)
 
+def existSense(senses, auxSense):
+	for i in range(len(senses)):
+		#print(senses[i][0])
+		if(auxSense==senses[i][0]):	return True;
+	return False;
 
 def procSentence(text):
     sz= len(text)
@@ -58,15 +68,17 @@ def strip_tags(html):
 def cleanSentence(text):
 	text=strip_tags(text)
 	text=text.replace("'" ,"")
+	text=text.replace("( . . . )" ,".")#split in sentences
+	text=text.replace('\ufeff' ,"") #just1 in utf-8
 	text=text.replace("& ;#8203;&#8203;" ,"") #just1
-	#lin=text.split(' ')	
-	print(text)
-	#print (lin)
+	return text
 	
-def readFile(fileName): 
-	with open(fileName,'r') as f:
+def readFile(fileName,encode): 
+	lines=[]
+	with open(fileName,'r',encoding=encode) as f:
 		for line in f:
-			cleanSentence(line)
+			lines.append(cleanSentence(line))
+	return lines	
     			
 #sentence = ["Vería" , "esa" , "película" , "una" , "y" , "otra" , "vez" , "sin" , "parar"]
 #sentence  = ["dime" , "si" , "te", "sientes" , "cansada"]
