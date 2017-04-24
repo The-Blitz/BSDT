36b9e3213570a1831d9e6f8cc4a13447c8efbf66
The-Blitz <a20112387@pucp.pe>
2017-04-24 02:44:49 -0500

    Leer lista de características

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import features as fe
 
+def readData(fileName):
+	data=[]
+	featList=[]
+	target=[]
+	cont=0
+	with open(fileName,'r',encoding='utf-8') as f:
+		for line in f:
+			feat = [l for l in line.split() if l]
+			if(cont==0):
+				for i in range(1,len(feat),2):
+					featList.append(feat[i]+ "-" +feat[i+1])
+			else:
+				auxList = dict()
+				target.append(feat[0])
+				for i in range(1,len(feat)):
+					auxList[featList[i-1]] = feat[i]	
+				data.append(auxList)	
+			cont+=1	
+	return data,target
 
 def main():
 	#TODO
-	features = fe.generate()
-	print("Nuevo Main")
+	fileName  = 'featList.txt'
+	data,target=readData(fileName)	
+
 
 if __name__ == "__main__":
     
