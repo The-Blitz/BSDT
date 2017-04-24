8faddc7834e8c4e6cc338702438e9f63f4cf3668
The-Blitz <a20112387@pucp.pe>
2017-04-24 06:02:12 -0500

    Prueba clasificador SVM

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import features as fe
+from sklearn.model_selection import train_test_split,cross_val_score,cross_val_predict
+from sklearn import datasets
+from sklearn import svm
+import numpy as np
+import pandas as pd
 
 def readData(fileName):
 	data=[]
@@ -15,15 +19,26 @@ def readData(fileName):
 				auxList = dict()
 				target.append(feat[0])
 				for i in range(1,len(feat)):
-					auxList[featList[i-1]] = feat[i]	
+					auxList[featList[i-1]] = int(feat[i])	
 				data.append(auxList)	
-			cont+=1	
-	return data,target
+			cont+=1
+	
+	npData=pd.DataFrame(data).values
+	return npData,target
 
 def main():
 	#TODO
 	fileName  = 'featList.txt'
 	data,target=readData(fileName)	
+	#X_train, X_test, y_train, y_test = train_test_split(data,target, test_size=0.40)
+	svc = svm.SVC(kernel='linear', C=1)#.fit(X_train, y_train) 
+	#ans_poly=svc.predict(X_test)
+	#acc_poly=svc.score(X_test,y_test)
+	#print (ans_poly ,"accuracy=", acc_poly)
+	scores = cross_val_score(svc, data, target, cv=5)
+	print ("Usando cross validation: " , ("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)))
+	result = cross_val_predict(svc, data, target, cv=5)
+	#print(result)
 
 
 if __name__ == "__main__":
