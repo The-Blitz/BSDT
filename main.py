cc32ff8c1a612cb3abdc4818599f1d60aa13dce4
The-Blitz <a20112387@pucp.pe>
2017-05-01 19:46:59 -0500

    Cambiar lectura de features

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from sklearn.decomposition import PCA
 
 import numpy as np
 import pandas as pd
+import features as fe
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -23,15 +24,12 @@ def readData(fileName):
 	with open(fileName,'r',encoding='utf-8') as f:
 		for line in f:
 			feat = [l for l in line.split() if l]
-			if(cont==0):
-				for i in range(1,len(feat),2):
-					featList.append(feat[i]+ "-" +feat[i+1])
-			else:
-				auxList = dict()
-				target.append(feat[0])
-				for i in range(1,len(feat)):
-					auxList[featList[i-1]] = int(feat[i])	
-				data.append(auxList)	
+			auxList = fe.createDict()
+			target.append(feat[0])
+			for i in range(1,len(feat),4):
+				feature = feat[i]+" " + feat[i+1] + " "+ feat[i+2]
+				auxList[feature] = int(feat[i+3])	
+			data.append(auxList)	
 			cont+=1
 	
 	npData=pd.DataFrame(data).values
@@ -46,12 +44,10 @@ def test_clasif(name,clasi,data,target):
 
 def main():
 	#TODO
-	fileName  = 'featList.txt'
+	fileName  = 'test.txt'
 	data,target=readData(fileName)
-	pca = PCA(n_components=40)
-	pca.fit(data)
-	pca_data = pca.transform(data)	
-	
+	print(data)
+	'''
 	svc = svm.SVC(kernel='linear', C=1)
 	lr  = LogisticRegression()
 	lda = LinearDiscriminantAnalysis()
@@ -64,7 +60,7 @@ def main():
 	test_clasif('svm',svc,data,target);test_clasif('logistic',lr,data,target);test_clasif('lda',lda,data,target);
 	test_clasif('k neighbors',knn,data,target);test_clasif('decision tree',tree,data,target);test_clasif('naive bayes',bayes,data,target);
 	test_clasif('sgd',sgd,data,target);
-
+	'''
 
 if __name__ == "__main__":
     
